fa171bc779f832df069c5d77049e895d00072a72
Bogdan Stevanovic <bogdan.stevanovic090204@gmail.com>
2024-06-01 15:30:47 +0200

    Is .gitignore now working???

---
+++ DesignPatterns/Observer/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Observer
+{
+    class Program
+    {
+        public static void Main(string[] args) { }
+    }
+    abstract class Subject
+    {
+        protected List<Observer> _observers;
+        public void AddObserver(Observer observee)
+        {
+            _observers.Add(observee);
+        }
+        public void RemoveObserver(Observer observee) {
+            _observers.Remove(observee);
+        }
+        public void notify()
+        {
+            foreach (Observer observer in _observers) {
+                observer.update();
+            }
+        }
+    }
+    abstract class Observer 
+    {
+        public abstract void update();
+    }
+    class ConcreteSubject : Subject
+    {
+        private char _state; // can be any type
+        public char getState() { return _state; }
+        public void setState(char state) { _state = state; }
+    }
+    class ConcreteObserver : Observer
+    {
+        private ConcreteSubject _subject;
+        private char _subjectState;
+        public ConcreteObserver()
+        {
+            _subject = new ConcreteSubject();
+            _subjectState = 'A'; // initial state, can be any
+        }
+        public override void update()
+        {
+            // logic goes here
+            _subjectState = _subject.getState();
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton
+{
+    class Singleton
+    {
+        private static Singleton _instance;
+        protected Singleton() { }
+        public static Singleton getInstance()
+        {
+            if(_instance == null)
+                _instance = new Singleton();
+            return _instance;
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.getInstance();
+            Singleton s2 = Singleton.getInstance();
+
+            if (s1 != s2)
+                throw new Exception("Not implemented correctly!\nTwo objects must be equal in Singleton pattern");
+            Console.WriteLine("Singleton pattern successfully implemented!");
+        }
+    }
+}
